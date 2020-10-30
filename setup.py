909b069f0692bde903d5b198a42c6257687809a1
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2020-10-30 20:26:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,20 +14,20 @@ plugin_package = "octoprint_filament_scale"
 plugin_name = "Filament Scale"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.3"
+plugin_version = "0.0.1b1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
 plugin_description = "Plugin for integrating a load cell into a filament holder."
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Victor Noordhoek"
+plugin_author = "eliteSCHW31N"
 
 # The plugin's author's mail address.
-plugin_author_email = "you@example.com"
+plugin_author_email = "business@eliteschw31n.de"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/dieki-n/OctoPrint-Filament-Scale"
+plugin_url = "https://github.com/eliteSchwein/Octoprint-Filament-Scale"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
