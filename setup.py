6a799b8ca3ee83d91314634c31d2ccff8600a3ca
eliteSCHW31N <admin@elitesch31n.de>
2020-10-31 16:37:42 +0100

    fix?

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/eliteSchwein/Octoprint-Filament-Scale"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["RPi.GPIO"]
+plugin_requires = ["RPi.GPIO,hx711"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
